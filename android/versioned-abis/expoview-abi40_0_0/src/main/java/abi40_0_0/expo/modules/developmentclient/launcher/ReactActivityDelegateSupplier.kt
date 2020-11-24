4dac1c1c8e06385dfa3c3054fba9e88e438607ba
Cedric van Putten <me@bycedric.com>
2020-11-24 21:22:10 +0100

    [android] Remove development client from ABI 40

--- android/versioned-abis/expoview-abi40_0_0/src/main/java/abi40_0_0/expo/modules/developmentclient/launcher/ReactActivityDelegateSupplier.kt
+++
@@ -1,8 +0,0 @@
-package abi40_0_0.expo.modules.developmentclient.launcher
-
-import abi40_0_0.com.facebook.react.ReactActivityDelegate
-
-@FunctionalInterface
-interface ReactActivityDelegateSupplier {
-  fun get(): ReactActivityDelegate
-}
